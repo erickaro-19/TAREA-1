0b911164575d74cd58290020fab12081ebc2e6be
Ericka Rocío Talé Aguilera <201800083@colsanjose.edu.gt>
2021-06-08 17:26:48 -0600

    inicio de tarea 1

---
+++ TAREA_1/TAREA_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREA_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] dato = new char[10];
+
+            //dato[0] = "a";
+
+            for (int i=0; i<=9; i++)
+            {
+                Console.WriteLine("i: " + i);
+                Console.Write("Ingrese una letra:");
+                i
+                    dato[i] = char.Parse(Console.ReadLine());
+
+
+            }
+        }
+    }
+}
