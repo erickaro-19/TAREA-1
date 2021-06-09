dc03df621ba2902e0d37453dea3c9807b46963b9
Ericka Rocío Talé Aguilera <201800083@colsanjose.edu.gt>
2021-06-08 18:17:30 -0600

    tarea iniciada

--- TAREA_1/TAREA_1/Program.cs
+++ TAREA_1/TAREA_1/Program.cs
@@ -5,24 +5,48 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TAREA_1
+{ 
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        char[] dato = new char[10];
+        char buscar;
+
+
+        //dato[0] = "a";
+
+        for (int i = 0; i <= 9; i++)
         {
-            char[] dato = new char[10];
-
-            //dato[0] = "a";
-
-            for (int i=0; i<=9; i++)
-            {
-                Console.WriteLine("i: " + i);
-                Console.Write("Ingrese una letra:");
-                i
-                    dato[i] = char.Parse(Console.ReadLine());
-
-
-            }
+            Console.WriteLine("i: " + i);
+            Console.Write("Ingrese una letra:");
+            dato[i] = char.Parse(Console.ReadLine());
         }
+        Console.WriteLine("-----------------");
+
+        for (int x = 0; x < 9; x++)
+        {
+            Console.WriteLine(dato[x]);
+
+        }
+        Console.WriteLine("--------------------------");
+
+        Console.Write("ingrese caracter a buscar:");
+        buscar = char.Parse(Console.ReadLine());
+
+        for (int z = 0; z <= 9; z++)
+        {
+            if (dato[z] == buscar)
+            {
+                Console.Write("dato" + buscar + " encontrado");
+            }
+
+            Console.ReadKey();
+
+        }
+
+
     }
+
+}
 }
